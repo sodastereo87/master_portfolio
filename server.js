1bf7bc5aa1d617628816c9cc7641295cb5d0c018
sodastereo <f_er_@hotmail.com>
2019-10-13 03:17:20 -0400

    repo updates

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require('express')
-// const routes = require('./routes')
 const app = express()
-const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 3001
 
-// Configure body parsing for AJAX requests
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-}
+app.use(cors())
+// here error
+app.use(bodyParser.json())
 
-// Add routes, both API and view
-// app.use(routes)
-
-// Start the API server
-app.listen(process.env.PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-)
+app.listen(PORT, function() {
+  console.log('server running on port:' + PORT)
+})
