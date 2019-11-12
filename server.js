145fc527ec40b846b20d84e84e377e8741138131
sodastereo <f_er_@hotmail.com>
2019-11-12 00:53:38 -0500

    old app

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const PORT = process.env.PORT || 3001
-
-app.use(cors())
-// here error
-app.use(bodyParser.json())
-
-app.listen(PORT, function() {
-  console.log('server running on port:' + PORT)
-})
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
